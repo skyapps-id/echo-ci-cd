f4390428288066a1f62d2cdac917df4e979b99dd
Aji Indra Jaya <aji.indrajaya@efishery.com>
2024-01-12 00:11:45 +0700

    update code #2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func helloHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World 123 !!!!")
+	return c.String(http.StatusOK, "Hello, World!")
 }
 
 func main() {
