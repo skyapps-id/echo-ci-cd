14cb2db325a77af3d041fd90c49e62e015e956bc
Aji Indra Jaya <aji.indrajaya@efishery.com>
2024-01-18 23:10:53 +0700

    update code #16

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func helloHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, eFishery 2024 !!!!")
+	return c.String(http.StatusOK, "Hello, eFishery !!!")
 }
 
 func main() {
